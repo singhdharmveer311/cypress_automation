ab19937ce28de147ac1fd823daa7dec0b5472899
Dharmveer Singh <46393548+singhdharmveer311@users.noreply.github.com>
2023-12-25 11:29:27 +0100

    commit 3

--- cypress/e2e/MyFirstTest.cy.js
+++ cypress/e2e/MyFirstTest.cy.js
@@ -1,20 +1,25 @@
-const cypress = require("cypress")
-
 
 /* 
-1. from terminal we can use `npx cypress open`  */
+1. from terminal we can use `npx cypress open`  
+2. `npx cypress run` to run directly from terminal, by default it pass headless mode, because it doesn't involve any UI
+2. If we want UI also then add --headed `npx cypress run --headed` -> this will direclt open the cypress app and u will see on UI
+4. For specific file -> `npx cypress run --spec` -> will run all the specs file
+    `npx cypress run cypress/e2e/MytestCase.cy.js`  or `npx cypress run cypress/e2e/MytestCase.cy.js --headed`
+
+*/
 
 describe('My First Test case', () => {
-  it('Test 1', () => {                                              // verify title
-    cy.visit("https://opensource-demo.orangehrmlive.com") 
+  it('Verify Title Positive Case', () => {                                              // Verify Title
+    cy.visit("https://opensource-demo.orangehrmlive.com/") 
 
     cy.title().should('eq', 'OrangeHRM')
   })
+  
 
-  it('Test 1', () => {                                              // verify title negative case
-    cy.visit("https://opensource-demo.orangehrmlive.com")
+  it('Verify Tiltle Negative Case', () => {                                              // Verify Title negative case
+    cy.visit("https://opensource-demo.orangehrmlive.com/")
 
-    cy.title().should('eq', 'OrangeHRM_Invalid')
+    cy.title().should('eq', 'OrangeHRM_Invalid111')
   })
 
 })
@@ -22,8 +27,8 @@ describe('My First Test case', () => {
 
 
 
-describe('My First Test', () => {           // In place of "=>" we can use "funtion()", because both are same in javascript
-    it('Does not do much!', () => {
-      expect(true).to.equal(true)
-    })
-  })
+// describe('My First Test', () => {           // In place of "=>" we can use "funtion()", because both are same in javascript
+//     it('Does not do much!', () => {
+//       expect(true).to.equal(true)
+//     })
+//   })
