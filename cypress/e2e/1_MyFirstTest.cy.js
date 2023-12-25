68111ef96ddfa1334bac265b42f2ee8704dab419
singhdharmveer311 <rs92539@gmail.com>
2023-12-25 12:40:56 +0100

    Last commit

--- cypress/e2e/1_MyFirstTest.cy.js
+++ cypress/e2e/1_MyFirstTest.cy.js
@@ -5,6 +5,12 @@
 2. If we want UI also then add --headed `npx cypress run --headed` -> this will direclt open the cypress app and u will see on UI
 4. For specific file -> `npx cypress run --spec` -> will run all the specs file
     `npx cypress run cypress/e2e/MytestCase.cy.js`  or `npx cypress run cypress/e2e/MytestCase.cy.js --headed`
+5. By default it will pick it's own browser if you need to specific browser then use , by default it is also headless mode
+    `npx cypress run --browser chrome` 
+
+
+    TestSuite ==== TestCase == Test Steps
+    Spec File(cy.js) --> Describe blocks(test suites ) -> It blocks ( test )
 
 */
 
@@ -26,26 +32,18 @@ describe('My First Test case', () => {
 
 
 
+describe('My First Test case', function() {           // In place of arrow you can give funtions so both are same in javascipt 
+  it('Verify Title Positive Case', function(){                                              // Verify Title
+    cy.visit("https://opensource-demo.orangehrmlive.com/") 
 
-// describe('My First Test', () => {           // In place of "=>" we can use "funtion()", because both are same in javascript
-//     it('Does not do much!', () => {
-//       expect(true).to.equal(true)
-//     })
-//   })
+    cy.title().should('eq', 'OrangeHRM')
+  })
+  
 
+  it('Verify Tiltle Negative Case', () => {                                              // Verify Title negative case
+    cy.visit("https://opensource-demo.orangehrmlive.com/")
 
+    cy.title().should('eq', 'OrangeHRM_Invalid')
+  })
 
-// describe('My First Test', () => {           // In place of "=>" we can use "funtion()", because both are same in javascript
-//     it('Does not do much!', () => {
-//       expect(true).to.equal(true)
-//     })
-//   })// describe('My First Test', () => {           // In place of "=>" we can use "funtion()", because both are same in javascript
-//     it('Does not do much!', () => {
-//       expect(true).to.equal(true)
-//     })
-//   })
-// describe('My First Test', () => {           // In place of "=>" we can use "funtion()", because both are same in javascript
-//     it('Does not do much!', () => {
-//       expect(true).to.equal(true)
-//     })
-//   })
+})
