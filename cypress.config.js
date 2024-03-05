75d76207cbe17caaf13e50601e7d06c46352834b
singhdharmveer311 <rs92539@gmail.com>
2024-03-05 01:29:36 +0100

    All changes of cypress complete

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',  // Fot HTML Report
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-    testIsolation: false
+      screenshotOnRunFailure=true;
+      require('cypress-mochawesome-reporter/plugin')(on); // for html eports
+    }
   },
 });
